2a6447d9456535a0216f1aade3f27f04638a9bcf
Roberto Cinetto <roberto.cinetto@gmail.com>
2020-01-27 14:30:35 +0100

    wpvuejstheme

---
+++ content/themes/wpvuejstheme/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>My website</title>
+</head>
+<body>
+    <h2>Content Title</h2>
+    <p>I just built my very first WordPress theme from scratch!</p>
+</body>
+</html>
